33003d49578493b207148ef24b47bffb9890b97f
JenniferCarmen <jennifercarmens@gmail.com>
2018-04-01 13:07:08 -0500

    login facebook google

---
+++ js/config_firebase.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBbx-mthvabXfbz383awvlcl5QBKcZ81R4",
+    authDomain: "heremaps-9f55c.firebaseapp.com",
+    databaseURL: "https://heremaps-9f55c.firebaseio.com",
+    projectId: "heremaps-9f55c",
+    storageBucket: "",
+    messagingSenderId: "534983346210"
+  };
+  firebase.initializeApp(config);
